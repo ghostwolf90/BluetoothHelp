f9b385024d6eacb64c6544675857aa51e9e2c74e
Laibit <ghostwolf90@gmail.com>
2018-05-06 13:55:37 +0800

    Initial Commit

---
+++ BluetoothHelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothHelp
+//
+//  Created by Laibit on 2018/5/6.
+//  Copyright © 2018年 Laibit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
